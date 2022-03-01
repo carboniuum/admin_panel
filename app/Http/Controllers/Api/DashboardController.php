e448c69cf4017cce8e1c98510ad64d35be7ff66c
Umarbek <umarbek@mail.uz>
2022-03-01 14:52:45 +0500

    docker configured

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -14,6 +14,19 @@ class DashboardController extends Controller
     {
         Gate::authorize('view', 'orders');
 
+        /* first way with eloquent */
+//        return Order::select(['id', 'created_at'])
+//            ->orderBy('created_at')
+//            ->get()
+//            ->transform(function ($order) {
+//            return [
+//                'created_at' => $order->created_at,
+//                'total' => $order->total,
+//            ];
+//        })
+//        ->toArray();
+
+        /* second way with joins */
         $orders = Order::query()
             ->join('order_items', 'orders.id', '=', 'order_items.order_id')
             ->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') as date, SUM(order_items.quantity * order_items.price) as sum")
